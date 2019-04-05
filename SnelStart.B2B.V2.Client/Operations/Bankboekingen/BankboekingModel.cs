f5c5263e6ebed28249ffefa10f2fd77030315290
Anton Hensen <ahensen@snelstart.nl>
2019-04-05 15:59:04 +0200

    Add comments to BankboekingModel

--- SnelStart.B2B.V2.Client/Operations/Bankboekingen/BankboekingModel.cs
+++ SnelStart.B2B.V2.Client/Operations/Bankboekingen/BankboekingModel.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace SnelStart.B2B.V2.Client.Operations
 {
-    
+
     public class BankboekingModel : IdentifierModel
     {
         /// <summary>
@@ -14,50 +14,41 @@ namespace SnelStart.B2B.V2.Client.Operations
         {
         }
 
-        
+        /// <summary>
+        /// Het tijdstip waarop de bankboeking is aangemaakt of voor het laatst is gewijzigd
+        /// </summary>
         public DateTime ModifiedOn { get; set; }
 
-        
-        
-        
         public DateTime Datum { get; set; }
 
-        
+        /// <summary>
+        /// Deze bankboeking verdient speciale aandacht, in SnelStart wordt dit visueel benadrukt.
+        /// </summary>
         public bool Markering { get; set; }
 
-        
-        
+        /// <summary>
+        /// Het boekstuknummer van de bankboeking.</summary>
         public string Boekstuk { get; set; }
 
-        
+        /// <summary>
+        /// Geeft aan of deze bankboeking is aangepast door de accountant.
+        /// </summary>
         public bool GewijzigdDoorAccountant { get; set; }
 
-        
-        
         public string Omschrijving { get; set; }
 
-        
         public GrootboekBoekingsRegelModel[] GrootboekBoekingsRegels { get; set; } = new GrootboekBoekingsRegelModel[0];
 
-        
         public InkoopBoekingVerantwoordingsRegelModel[] InkoopboekingBoekingsRegels { get; set; } = new InkoopBoekingVerantwoordingsRegelModel[0];
 
-        
         public VerkoopBoekingVerantwoordingsRegelModel[] VerkoopboekingBoekingsRegels { get; set; } = new VerkoopBoekingVerantwoordingsRegelModel[0];
 
-        
         public BtwBoekingregelModel[] BtwBoekingsregels { get; set; } = new BtwBoekingregelModel[0];
 
-        
-        
         public decimal BedragUitgegeven { get; set; }
 
-        
-        
         public decimal BedragOntvangen { get; set; }
 
-        
-        
-        public DagboekIdentifierModel Dagboek{ get; set; }
+        public DagboekIdentifierModel Dagboek { get; set; }
     }
 }
