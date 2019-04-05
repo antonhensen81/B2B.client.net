c3f3f5a3d8e3e19fd203fa95ce3a6be2f2453bb3
Anton Hensen <ahensen@snelstart.nl>
2019-04-05 15:53:34 +0200

    Update MemoriaalboekingModel

--- SnelStart.B2B.V2.Client/Operations/Memoriaalboekingen/MemoriaalboekingModel.cs
+++ SnelStart.B2B.V2.Client/Operations/Memoriaalboekingen/MemoriaalboekingModel.cs
@@ -1,6 +1,4 @@
 using System;
-using SnelStart.B2B.V2.Client.Operations;
-
 
 namespace SnelStart.B2B.V2.Client.Operations
 {
@@ -13,7 +11,7 @@ namespace SnelStart.B2B.V2.Client.Operations
         /// Geeft de naam van deze gegevenscontainer terug.
         /// </summary>
         public const string ResourceName = "memoriaalboekingen";
-        
+
         /// <summary>
         /// Geeft een instantie van een <see cref="MemoriaalboekingModel"/> terug.
         /// </summary>
@@ -21,55 +19,45 @@ namespace SnelStart.B2B.V2.Client.Operations
         {
         }
 
-        
-        public DateTime ModifiedOn { get; set; }
-
-        
-        
-        
-        public DateTime Datum { get; set; }
-
-        
-        public bool Markering { get; set; }
-
-        
-        
-        public string Boekstuk { get; set; }
-
-        
-        public bool GewijzigdDoorAccountant { get; set; }
-
-
-        
-        
-        public string Omschrijving { get; set; }
-
-        
-        public MemoriaalBoekingsRegelModel[] MemoriaalBoekingsRegels { get; set; } = new MemoriaalBoekingsRegelModel[0];
-
-        
-        public InkoopBoekingVerantwoordingsRegelModel[] InkoopboekingBoekingsRegels { get; set; } = new InkoopBoekingVerantwoordingsRegelModel[0];
-
-        
-        public VerkoopBoekingVerantwoordingsRegelModel[] VerkoopboekingBoekingsRegels { get; set; } = new VerkoopBoekingVerantwoordingsRegelModel[0];
-
-        
-        
-        public DagboekIdentifierModel Dagboek { get; set; }
 
         /// <summary>
-        /// 
+        /// Het tijdstip waarop de memoriaalboeking is aangemaakt of voor het laatst is gewijzigd
         /// </summary>
+        public DateTime ModifiedOn { get; set; }
+
+        public DateTime Datum { get; set; }
+
+        /// <summary>
+        /// Deze memoriaalboeking verdient speciale aandacht, in SnelStart wordt dit visueel benadrukt.
+        /// </summary>
+        public bool Markering { get; set; }
+
+        /// <summary>
+        /// Het boekstuknummer van de memoriaalboeking.</summary>
+        public string Boekstuk { get; set; }
+
+        /// <summary>
+        /// Geeft aan of deze memoriaalboeking is aangepast door de accountant.
+        /// </summary>
+        public bool GewijzigdDoorAccountant { get; set; }
+
+        public string Omschrijving { get; set; }
+
+        public MemoriaalBoekingsRegelModel[] MemoriaalBoekingsRegels { get; set; } = new MemoriaalBoekingsRegelModel[0];
+
+        public InkoopBoekingVerantwoordingsRegelModel[] InkoopboekingBoekingsRegels { get; set; } = new InkoopBoekingVerantwoordingsRegelModel[0];
+
+        public VerkoopBoekingVerantwoordingsRegelModel[] VerkoopboekingBoekingsRegels { get; set; } = new VerkoopBoekingVerantwoordingsRegelModel[0];
+
+        public DagboekIdentifierModel Dagboek { get; set; }
+
         public class MemoriaalBoekingsRegelModel
         {
-            
-            
             public string Omschrijving { get; set; }
 
             /// <summary>
             /// Verwijzing naar een grootboek
             /// </summary>
-            
             public GrootboekIdentifierModel Grootboek { get; set; }
 
             /// <summary>
@@ -77,22 +65,16 @@ namespace SnelStart.B2B.V2.Client.Operations
             /// </summary>
             public KostenplaatsIdentifierModel Kostenplaats { get; set; }
 
-            
             public decimal Debet { get; set; }
 
-            
             public decimal Credit { get; set; }
         }
 
-        
+
         public class VerantwoordingsRegelModel
         {
-            
-            
             public string Omschrijving { get; set; }
 
-            
-            
             public decimal Bedrag { get; set; }
         }
 
@@ -104,7 +86,6 @@ namespace SnelStart.B2B.V2.Client.Operations
             /// <summary>
             /// Verwijzing naar een inkoopboeking
             /// </summary>
-            
             public InkoopboekingIdentifierModel BoekingId { get; set; }
         }
 
@@ -116,7 +97,6 @@ namespace SnelStart.B2B.V2.Client.Operations
             /// <summary>
             /// Verwijzing naar een verkoopboeking
             /// </summary>
-            
             public VerkoopboekingIdentifierModel BoekingId { get; set; }
         }
     }
